1e68359966644be511b16943cec1ab811cf15fdd
ToineTRGR <toine@trgr.nl>
2026-02-25 16:19:17 +0100

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Visual Design System",
+  description: "Digital-first scalable visual identity system",
+
+  themeConfig: {
+    sidebar: [
+      {
+        text: "Design System",
+        items: [
+          { text: "Overview", link: "/design-system/" }
+        ]
+      }
+    ]
+  }
+}
