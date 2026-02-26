c5e2013968af15005ab457eabc8eaaf72f84c662
ToineTRGR <toine@trgr.nl>
2026-02-26 12:50:54 +0100

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,12 @@
 export default {
+  base: "/Visual-Design-Guidelines/",
   title: "Visual Design System",
   description: "Digital-first scalable visual identity system",
-
   themeConfig: {
     sidebar: [
       {
         text: "Design System",
         items: [
-          { text: "Overview", link: "/design-system/" },
           { text: "Colors", link: "/design-system/colors" },
           { text: "Typography", link: "/design-system/typography" }
         ]
